8734abe46898f5c76e8ec884261c76d64d9888f5
aleks <aleksei-davletov@mail.ru>
2023-08-30 17:56:06 +0300

    Добавил @ResponseStatus()

--- src/main/java/pro/sky/HomeWork/exception/EmployeeStorageIsFullException.java
+++ src/main/java/pro/sky/HomeWork/exception/EmployeeStorageIsFullException.java
@@ -1,5 +1,8 @@
 package pro.sky.HomeWork.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus()
 public class EmployeeStorageIsFullException extends RuntimeException{
     public EmployeeStorageIsFullException(String message) {
         super(message);
